c42f83d998dbf854fe4c2db04a491395ac19f624
OskarasKub <112699253+OskarasKub@users.noreply.github.com>
2022-09-30 01:53:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,17 +17,6 @@ using std::fixed;
 using std::setprecision;
 using std::vector;
 
-struct Studentas {
-    string vardas; //Studento vardas
-    string pavarde; //Studento pavarde
-    int n=0; //Namu darbu kiekis
-    vector<int> paz; //Namu darbu pazymiai
-    int egz; //Egzamino pazymys
-    double vidurkis; //Pazymiu vidurkis
-    double mediana; //Pazymiu mediana
-    int sum=0; //Pazymiu suma
-};
-
 void sugeneravimas(vector<Studentas>& A, int x) {
     srand((unsigned int)time(0));
 
@@ -45,49 +34,6 @@ void sugeneravimas(vector<Studentas>& A, int x) {
         cout << "Sugeneruotas " << i + 1 << "-asis pazymys: " << rPaz << endl;
         A[x].paz[i] = rPaz;
         A[x].sum += rPaz;
-    }
-}
-
-void vidurkis(vector<Studentas>& A, int x) {
-    if (A[x].n > 0) {
-        A[x].vidurkis = double(A[x].sum) / double(A[x].n) * 0.40 + double(A[x].egz) * 0.60;
-    }
-    else {
-        A[x].vidurkis = double(A[x].egz) * 0.60;
-    }
-}
-
-void mediana(vector<Studentas>& A, int x){
-
-    int t;
-    int p = A[x].n;
-    A[x].paz.push_back(1);
-
-    for (int i = p; i > 0; i--) {
-        A[x].paz[i] = A[x].paz[i - 1];
-    }
-
-    A[x].paz[0] = A[x].egz;
-    p++;
-
-    for (int i = 0; i < p; i++) {
-        for (int j = 0; j < p - i - 1; j++) {
-            if (A[x].paz[j] < A[x].paz[j + 1]) {
-                t = A[x].paz[j];
-                A[x].paz[j] = A[x].paz[j + 1];
-                A[x].paz[j + 1] = t;
-            }
-        }
-    }
-
-    if (p == 0) {
-        A[x].mediana = A[x].paz[p];
-    }
-    else if (p % 2 == 0) {
-        A[x].mediana = (A[x].paz[p / 2] + A[x].paz[p / 2 - 1]) / 2.0;
-    }
-    else {
-        A[x].mediana = A[x].paz[p / 2];
     }
 }
 
