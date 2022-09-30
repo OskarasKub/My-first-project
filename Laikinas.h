f5aded1ea0467571e34ad08c4bfa26e016841cdd
OskarasKub <112699253+OskarasKub@users.noreply.github.com>
2022-09-30 09:07:15 +0300

    Add files via upload

---
+++ Laikinas.h
@@ -0,0 +1,9 @@
+#include <iostream>
+using std::string;
+
+struct Laikinas {
+    string vardas; //Studento vardas
+    string pavarde; //Studento pavarde
+    double vidurkis; //Pazymiu vidurkis
+    double mediana; //Pazymiu mediana
+};
