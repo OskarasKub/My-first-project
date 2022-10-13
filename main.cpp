a32ba301b37e22f0d41dd97475ada9e39e8d5998
OskarasKub <112699253+OskarasKub@users.noreply.github.com>
2022-10-14 02:28:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,46 +2,30 @@
 #include <string>
 #include <iomanip>
 #include <vector>
+#include <fstream>
 #include "Studentas.h"
-#include "Funkcijos.h"
+#include "Sugeneravimas.h"
 
 using std::cout;
 using std::cin;
 using std::string;
 using std::endl;
 using std::vector;
+using std::ofstream;
 
 void vidurkis(vector<Studentas>& A, int x);
 void mediana(vector<Studentas>& A, int x);
-void sugeneravimas(vector<Studentas>& A, int x);
-void ivedimas(vector<Studentas>& A, int& kiek);
-void nuskaitymas(vector<Studentas>& A, int& kiek);
-void pavardziu_rusiavimas(vector<Studentas>& A, int kiek);
-void isvedimas(vector<Studentas> A, int kiek);
+void pazymiai(vector<Studentas>& A, int x);
+void studentai(vector<Studentas>& A, int kiek);
+void failai(vector<Studentas>& A, int& kiek);
+
 
 int main() {
 
     vector<Studentas> A;
     int kiek;
 
-    string x; //Ar vedimas ranka ar is failo
-    cout << "\nAr duomenis ivesite patys ar nuskaitysite is failo? (T- taip; N - ne, nuskaitysiu is failo): "; cin >> x;
-    while (x != "T" && x != "t" && x != "N" && x != "n") {
-        cout << "Kazka ne taip ivedete!" << endl;
-        cin.clear();
-        cin.ignore(256, '\n');
-        cout << "Iveskite T, jei norite patys ivesti duomenis arba N, jei duomenys nuskaitysite is failo: "; cin >> x;
-        continue;
-    }
-
-    if (x == "T" || x == "t") {
-        ivedimas(A, kiek);
-    }
-    else {
-        nuskaitymas(A, kiek);
-    }
-
-    isvedimas(A, kiek);
+    failai(A, kiek);
 
     return 0;
 }
