6bfe01a95acec0286061ddac96c1422e84f5ffae
OskarasKub <112699253+OskarasKub@users.noreply.github.com>
2022-10-14 10:52:56 +0300

    Delete Laikinas.h

--- Laikinas.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using std::string;
-
-struct Laikinas {
-    string vardas; //Studento vardas
-    string pavarde; //Studento pavarde
-    double vidurkis; //Pazymiu vidurkis
-    double mediana; //Pazymiu mediana
-};
